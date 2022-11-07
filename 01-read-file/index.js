3ec0de0f8919b012d57ec8633b2e54873b52f5f6
DESKTOP-7E3MSIO\ekate <ekaterinadanilenko95@gmail.com>
2022-11-08 00:21:27 +0300

    refactor: added changes

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,3 +1,4 @@
 const fs = require('fs');
-const readStream = fs.createReadStream('text.txt', 'utf-8');
-readStream.on('data', chunk => console.log(chunk));
+const path = require('path');
+const readStream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
+readStream.on('data', data => console.log(data));
