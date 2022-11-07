81a8ee585486f89c7730b4712c1d207b86ba2840
DESKTOP-7E3MSIO\ekate <ekaterinadanilenko95@gmail.com>
2022-11-08 00:33:30 +0300

    refactor: added changes

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+const mkdir = require('fs');
+const copyFile = require('fs');
+
+
+  fs.mkdir('D:\\htmlcss\\HTML-builder\\04-copy-directory\files-copy', (error) => {
+    if(error) {
+      throw error;
+    }})
+    
 
