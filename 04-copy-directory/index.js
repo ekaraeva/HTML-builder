cfc5f7b092402af24310a1d37d49dfa9c48530e0
ekaraeva <ekaterinadanilenko95@gmail.com>
2022-11-08 12:21:56 +0300

    feat: added 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,11 +2,31 @@ const fs = require('fs');
 const path = require('path');
 const mkdir = require('fs');
 const copyFile = require('fs');
+const { error } = require('console');
+const folder = path.join(__dirname, 'files');
 
-
-  fs.mkdir('D:\\htmlcss\\HTML-builder\\04-copy-directory\files-copy', (error) => {
+function copyDir () {
+  fs.mkdir(path.join(__dirname, 'files-copy'),{recursive: true}, (error) => {
     if(error) {
       throw error;
-    }})
-    
+    }
+    console.log('folder added')
+  })
+  fs.readdir(folder, (error, files) => {
+    if(error){
+      throw error;
+    }
+    else{  
+    files.forEach((file) => {
+        fs.promises.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file));
+    })
+
+}})
+  
+  }
+  copyDir();
+
+  
+
+
 
